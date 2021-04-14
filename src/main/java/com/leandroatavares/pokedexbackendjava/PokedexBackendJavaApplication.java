61ba63964a05627c001f9feeeb3f8880862e3696
Original <leandro.tavares@original.com.br>
2021-04-13 22:14:07 -0300

    :boom:  - start project

---
+++ src/main/java/com/leandroatavares/pokedexbackendjava/PokedexBackendJavaApplication.java
@@ -0,0 +1,13 @@
+package com.leandroatavares.pokedexbackendjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokedexBackendJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokedexBackendJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/leandroatavares/pokedexbackendjava/PokedexBackendJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leandroatavares.pokedexbackendjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokedexBackendJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
